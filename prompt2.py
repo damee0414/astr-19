aa7112f1522955e7099e21948f8b9f9e37148c8c
damee0414 <104191095+damee0414@users.noreply.github.com>
2022-04-22 00:56:39 -0700

    Add files via upload

---
+++ prompt2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def main():
+	a = 12.3
+	b = 23.4
+	print(a+b)
+	print(type(a+b))
+
+	c = 12
+	d = 9
+	print(c-d)
+	print(type(c-d))
+
+	print(a*c)
+	print(type(a*c))
+
+if __name__=="__main__":
+	main()
